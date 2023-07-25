56ad5e2b47d71e812a7ec94b0233b48b2c1bc2e9
hugo cm <hugocmdesign@gmail.com>
2023-07-25 23:40:11 +0200

    Add stage pipelines and a lambda

--- lib/cicd-aws-cdk-stack.ts
+++ lib/cicd-aws-cdk-stack.ts
@@ -5,17 +5,21 @@ import {
   ShellStep,
 } from 'aws-cdk-lib/pipelines';
 import { Construct } from 'constructs';
+import { PipelineStage } from './PipelineStage';
 
 export class CicdAwsCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    new CodePipeline(this, 'AwesomePipeline', {
+    const pipeline = new CodePipeline(this, 'AwesomePipeline', {
       pipelineName: 'AwesomePipeline',
       synth: new ShellStep('Synth', {
         input: CodePipelineSource.gitHub('UserName/RepoName', 'BranchName'),
         commands: ['npm ci', 'npx cdk synth'],
       }),
     });
+    const firtsStage = pipeline.addStage(
+      new PipelineStage(this, 'PipelineFirstStage', { stageName: 'First' })
+    );
   }
 }
